c4134bba23725b71cdd4f4de78ecb30683e904b1
sambamou <sambamou@uqtr.ca>
2022-03-27 17:03:51 -0400

    creation de la classe Drug

---
+++ src/app/model/Drug.ts
@@ -0,0 +1,31 @@
+export  interface Drug{
+   drug_number:number;
+
+   cat_number:number;
+
+  dci:String;
+
+   dosage:String ;
+
+   form:String ;
+
+  cases:number;
+
+   posts:number;
+
+   centers:number;
+
+  reference:String;
+
+  eps1:number;
+
+ eps2:number;
+
+  eps3:number;
+
+ createdBy:String;
+
+  lastUpdatedBy:String;
+
+  status:number;
+}
