f2d06d0c904bee6f965c84a9a0ecc7d66b99464b
BelkadiZine <zine.eddine.belkadi@uqtr.ca>
2022-03-29 12:01:41 -0400

    udpate Login & Inscription + Menu & Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {AppModuleComponentModule} from "../app-module-component/app-module-component.module";
 import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
